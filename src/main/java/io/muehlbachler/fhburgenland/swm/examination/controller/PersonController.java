34b5d2dd40927a82d373f7e4e6e9061ead971554
mrtndwrk <martin.dworak1997@gmail.com>
2024-02-02 18:24:31 +0100

    doc: updated javadoc for PersonController, relates to #1

--- src/main/java/io/muehlbachler/fhburgenland/swm/examination/controller/PersonController.java
+++ src/main/java/io/muehlbachler/fhburgenland/swm/examination/controller/PersonController.java
@@ -22,26 +22,53 @@ public class PersonController {
     @Autowired
     private PersonService personService;
 
+
+    /**
+     * retrieves all persons
+     * @return list of all persons
+     */
     @GetMapping("/")
     public List<Person> list() {
         return personService.getAll();
     }
 
+    /**
+     * retrieves a person by its unique id
+     * @param id the unique id of the person
+     * @return response entity with the person, or empty if the person doesnt exist
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Person> get(@PathVariable String id) {
         return ResponseEntity.of(personService.get(id));
     }
 
+    /**
+     * creates a new person
+     * @param person the person to create
+     * @return the created person
+     */
     @PostMapping("/")
     public Person create(@RequestBody Person person) {
         return personService.create(person);
     }
 
+    /**
+     * retrieves all persons based on a specific name query
+     * @param firstName the first name to search for
+     * @param lastName the last name to search for
+     * @return list of persons that match the query
+     */
     @GetMapping("/query")
     public List<Person> query(@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName) {
         return personService.findByName(firstName, lastName);
     }
 
+    /**
+     * creates a new note for a person
+     * @param id the unique id of the person
+     * @param note the note to create
+     * @return the created note
+     */
     @PostMapping("/{id}/note")
     public ResponseEntity<Note> createNote(@PathVariable String id, @RequestBody Note note) {
         return ResponseEntity.of(personService.createNote(id, note));
