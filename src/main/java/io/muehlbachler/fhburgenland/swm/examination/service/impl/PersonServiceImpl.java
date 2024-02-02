0bee88b034b1084abc5b6cbf7c0252e54e579292
mrtndwrk <martin.dworak1997@gmail.com>
2024-02-02 18:30:45 +0100

    doc: added javadoc for PersonServiceImpl, relates to #1

--- src/main/java/io/muehlbachler/fhburgenland/swm/examination/service/impl/PersonServiceImpl.java
+++ src/main/java/io/muehlbachler/fhburgenland/swm/examination/service/impl/PersonServiceImpl.java
@@ -25,19 +25,40 @@ public class PersonServiceImpl implements PersonService {
     @Autowired
     private NoteService noteService;
 
+    /**
+     * retrieves a list of all persons
+     * @return list of all person entities
+     */
     public List<Person> getAll() {
         return Lists.newArrayList(personRepository.findAll());
     }
 
+    /**
+     * retrieves a person by its unique id
+     * @param id the unique id of the person
+     * @return response entity with the person, or empty if the person doesnt exist
+     */
     public Optional<Person> get(String id) {
         return personRepository.findById(id);
     }
 
+    /**
+     * creates a new person
+     * @param person the person to create
+     * @return the created person
+     */
     @Override
     public Person create(Person person) {
         return personRepository.save(person);
     }
 
+
+    /**
+     * retrieves all persons based on a specific name query
+     * @param firstName the first name to search for
+     * @param lastName the last name to search for
+     * @return list of persons that match the query
+     */
     @Override
     public List<Person> findByName(String firstName, String lastName) {
         if (firstName.isEmpty() && !lastName.isEmpty()) {
@@ -48,6 +69,12 @@ public class PersonServiceImpl implements PersonService {
         return Lists.newArrayList();
     }
 
+    /**
+     * creates a new note for a person
+     * @param personId the unique id of the person
+     * @param note the note to create
+     * @return the created note
+     */
     @Override
     public Optional<Note> createNote(String personId, Note note) {
         return get(personId).map((Person person) -> {
