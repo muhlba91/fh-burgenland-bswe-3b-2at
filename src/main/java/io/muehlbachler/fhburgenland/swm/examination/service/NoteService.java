238330a00e580acd7ff5abc05c9951e72dd88788
mrtndwrk <martin.dworak1997@gmail.com>
2024-02-02 18:31:43 +0100

    doc: added javadoc for NoteService, relates to #1

--- src/main/java/io/muehlbachler/fhburgenland/swm/examination/service/NoteService.java
+++ src/main/java/io/muehlbachler/fhburgenland/swm/examination/service/NoteService.java
@@ -6,9 +6,25 @@ import java.util.Optional;
 import io.muehlbachler.fhburgenland.swm.examination.model.Note;
 
 public interface NoteService {
+
+    /**
+     * retrieves a note by its unique id
+     * @param id the unique id of the note
+     * @return response entity with the note, or empty if the note doesnt exist
+     */
     Optional<Note> get(String id);
 
+    /**
+     * creates a new note
+     * @param note the note to create
+     * @return the created note
+     */
     Note create(Note note);
 
+    /**
+     * retrieves all notes based on a specific content query
+     * @param query the query to search for
+     * @return list of notes that match the query
+     */
     List<Note> queryByContent(String query);
 }
