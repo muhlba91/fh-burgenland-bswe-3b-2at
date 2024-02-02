abe0989268d4ea7a05c6734ac42777a36888a305
mrtndwrk <martin.dworak1997@gmail.com>
2024-02-02 18:40:41 +0100

    doc: added javadoc for NoteRepository and PersonRepository, relates to #1

--- src/main/java/io/muehlbachler/fhburgenland/swm/examination/repository/NoteRepository.java
+++ src/main/java/io/muehlbachler/fhburgenland/swm/examination/repository/NoteRepository.java
@@ -7,5 +7,11 @@ import org.springframework.data.repository.CrudRepository;
 import io.muehlbachler.fhburgenland.swm.examination.model.Note;
 
 public interface NoteRepository extends CrudRepository<Note, String> {
+
+    /**
+     * retrieves all notes based on a specific content query
+     * @param content the content to search for
+     * @return list of notes that match the query
+     */
     List<Note> findByContentContaining(String content);
 }
--- src/main/java/io/muehlbachler/fhburgenland/swm/examination/repository/PersonRepository.java
+++ src/main/java/io/muehlbachler/fhburgenland/swm/examination/repository/PersonRepository.java
@@ -7,9 +7,26 @@ import org.springframework.data.repository.CrudRepository;
 import io.muehlbachler.fhburgenland.swm.examination.model.Person;
 
 public interface PersonRepository extends CrudRepository<Person, String> {
+
+    /**
+     * retrieves all persons based on a specific first name
+     * @param firstName the first name to search for
+     * @return list of persons that match the query
+     */
     List<Person> findByFirstName(String firstName);
 
+    /**
+     * retrieves all persons based on a specific last name
+     * @param lastName the last name to search for
+     * @return list of persons that match the query
+     */
     List<Person> findByLastName(String lastName);
 
+    /**
+     * retrieves all persons based on a specific first and last name query
+     * @param firstName the first name to search for
+     * @param lastName the last name to search for
+     * @return list of persons that match the query
+     */
     List<Person> findByFirstNameAndLastName(String firstName, String lastName);
 }
