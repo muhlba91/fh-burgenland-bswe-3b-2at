926c3a254a8a3d8e8d9baac593230dd21afb0375
asirbescu-fhbg <114183627+asirbescu-fhbg@users.noreply.github.com>
2024-02-10 13:51:46 +0100

    docs: add javadoc to repository methods (issue: #14) (#18)
    docs(note): add javadoc to repository methods (issue: #14)
    
    docs(person): add javadoc to repository methods (issue: #14)

--- src/main/java/io/muehlbachler/fhburgenland/swm/examination/repository/NoteRepository.java
+++ src/main/java/io/muehlbachler/fhburgenland/swm/examination/repository/NoteRepository.java
@@ -2,10 +2,21 @@ package io.muehlbachler.fhburgenland.swm.examination.repository;
 
 import java.util.List;
 
+import io.muehlbachler.fhburgenland.swm.examination.service.NoteService;
 import org.springframework.data.repository.CrudRepository;
 
 import io.muehlbachler.fhburgenland.swm.examination.model.Note;
 
 public interface NoteRepository extends CrudRepository<Note, String> {
+
+    /**
+     * Retrieves a list of Note entities whose content contains the provided
+     * content.
+     * @param content The string for which the content is checked.
+     * @return List of Notes whose content matches the provided content or an
+     *         empty list if no Notes whose content matches the provided
+     *         content are found or the provided content is null.
+     *         Returns a list with all Notes if the provided content is empty.
+     */
     List<Note> findByContentContaining(String content);
 }
--- src/main/java/io/muehlbachler/fhburgenland/swm/examination/repository/PersonRepository.java
+++ src/main/java/io/muehlbachler/fhburgenland/swm/examination/repository/PersonRepository.java
@@ -7,9 +7,42 @@ import org.springframework.data.repository.CrudRepository;
 import io.muehlbachler.fhburgenland.swm.examination.model.Person;
 
 public interface PersonRepository extends CrudRepository<Person, String> {
+
+    /**
+     * Retrieves a list of Person entities whose first name matches the given
+     * first name (case-sensitive).
+     * @param firstName The string for which the first name is checked.
+     * @return List of Person entities whose first name matches the provided
+     *         first name or an empty list if no Person entities whose first
+     *         name matches the provided first name are found or the provided
+     *         first name is null. Returns a list with all Person entities if
+     *         the provided first name is empty.
+     */
     List<Person> findByFirstName(String firstName);
 
+    /**
+     * Retrieves a list of Person entities whose last name matches the given
+     * last name (case-sensitive).
+     * @param lastName The string for which the last name is checked.
+     * @return List of Person entities whose last name matches the provided
+     *         last name or an empty list if no Person entities whose last
+     *         name matches the provided last name are found or the provided
+     *         last name is null. Returns a list with all Person entities if
+     *         the provided last name is empty.
+     */
     List<Person> findByLastName(String lastName);
 
+    /**
+     * Retrieves a list of Person entities whose first and last name matches
+     * the given parameters (case-sensitive).
+     * @param firstName The string for which the first name is checked.
+     * @param lastName The string for which the last name is checked.
+     * @return List of Person entities whose first and last name matches the
+     *         provided first and last name or an empty list if no Person entities
+     *         whose first and last name matches the provided first and last
+     *         name are found or the provided first and last name are null.
+     *         Returns a list with all Person entities if the provided first
+     *         and last name is empty.
+     */
     List<Person> findByFirstNameAndLastName(String firstName, String lastName);
 }
